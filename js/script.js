364a6fb141a7700fcb2da6f12086a891c4b2c63a
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-06-27 17:01:28 +0200

    completed + bonus

--- js/script.js
+++ js/script.js
@@ -1,29 +1,77 @@
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
+var app = new Vue({
+    el: '#root',
+    data: {
+        slideCounter: 0,
 
-console.log(slides);
+        autoPlayClock: null,
+
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ],
+    },
+
+    methods: {
+
+        counterUp() {
+            if( this.slideCounter < this.slides.length -1 ) {
+                this.slideCounter += 1;
+            } else {
+                this.slideCounter = 0;
+            }
+        },
+
+        counterDown() {
+            if( this.slideCounter === 0) {
+                this.slideCounter = this.slides.length -1;
+            } else {
+                this.slideCounter -= 1;
+            }
+        },
+
+        counterThumb(a) {
+            this.slideCounter = a;
+        },
+
+        stopAutoplay() {
+            clearInterval(this.autoPlayClock);
+            this.autoPlayClock = null;
+        },
+
+        startAutoplay() {
+            this.autoPlayClock = setInterval(this.counterUp, 3000)
+        }
+    },
+
+    mounted() {
+        this.startAutoplay();
+    }
+
+  })
+
+
+
+
